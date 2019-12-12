b278008acc46c4f746c388bb0c24fb5db428db97
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-12 17:02:41 +0530

    ADDED: Check for NoMove,Snake and Ladder

--- SnakeAndLadderGame.sh
+++ SnakeAndLadderGame.sh
@@ -3,5 +3,20 @@
 echo "Welcome to Snake And Ladder Game"
 
 START_POSITION=0
+No_Move=0
+SNAKE=1
+LADDER=2
 
-dieRoll=((1+RANDOM%6))
+dieRoll=$((1+RANDOM%6))
+position=0
+msl=$((RANDOM%3))
+
+case $msl in
+
+	$NO_MOVE)
+			echo "No Move";;
+	$SNAKE)
+			position=$(( $position - $dieRoll ));;
+	$LADDER)
+			position=$(( $position + $dieRoll ));;
+esac
