553ddc397fb37dd1c09c447b76a81486d1f982aa
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-14 12:39:19 +0530

    ADDED: Function To Check Who Is Win Game

--- SnakeAndLadderGame.sh
+++ SnakeAndLadderGame.sh
@@ -10,36 +10,55 @@ LADDER=2
 position=0
 record=1
 
-declare -A gameRecord
+function checkWhoIsWinner(){
 
-while (( $position < 100 ))
-do
+	while (( $position < 100 ))
+	do
 
-	dieRoll=$((1+RANDOM%6))
-	playerMove=$((RANDOM%3))
+		dieRoll=$((1+RANDOM%6))
+		playerMove=$((RANDOM%3))
 
-	case $playerMove in
+		case $playerMove in
 
-		$NO_MOVE)
-				echo "No Move";;
-		$SNAKE)
-				position=$(( $position - $dieRoll ))
+			$NO_MOVE)
+					echo "No Move";;
 
-				if (( $position < $START_POSITION ))
-				then
-						position=$START_POSTITION
-				fi;;
-		$LADDER)
-				position=$(( $position + $dieRoll ))
+			$SNAKE)
+					position=$(( $position - $dieRoll ))
 
-				if (( $position > 100 ))
-				then
+					if (( $position < $START_POSITION ))
+					then
+						position=$START_POSITION
+					fi;;
+
+			$LADDER)
+					position=$(( $position + $dieRoll ))
+
+					if (( $position > 100 ))
+					then
 						position=$(( $position - $dieRoll ))
-				fi;;
-	esac
+					fi;;
+		esac
 
-		gameRecord[$record]=$position
-		record=$(($record + 1 ))
-done
+			totalDice=$(( $totalDice + 1 ))
+	done
 
-echo ${#gameRecord[@]}
+	echo $totalDice
+}
+
+function main(){
+
+	player1=$( checkWhoIsWinner )
+	player2=$( checkWhoIsWinner )
+
+	echo "Player 1 Total Dice $player1"
+	echo "Player 2 Total Dice $player2"
+	if(( $player1 < $player2 ))
+	then
+		echo "Player 1 Winner $player1"
+	else
+		echo "Player 2 Winner $player2"
+	fi
+}
+
+main
